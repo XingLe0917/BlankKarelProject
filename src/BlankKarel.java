4e5406afc5d5c670ddb3988d72e4100caff4beeb
xinle <xingle@sportsdata.cn>
2017-08-11 16:26:04 +0800

    add files

---
+++ src/BlankKarel.java
@@ -0,0 +1,21 @@
+/*
+ * File: BlankKarel.java
+ * ---------------------
+ * This class is a blank one that you can change to whatever you want!
+ */
+
+import stanford.karel.*;
+
+public class BlankKarel extends SuperKarel {
+	
+	
+	public void run() {
+		/* You fill this in */
+		move();
+		turnLeft();
+		move();
+		turnRight();
+		move();
+	}
+}
+
